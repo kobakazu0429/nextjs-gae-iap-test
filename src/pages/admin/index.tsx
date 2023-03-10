334d425719d21049a56d5a40c02203852fe8753b
kobakazu0429 <kobakazu10@gmail.com>
2023-03-10 11:03:16 +0900

    add admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Admin() {
+  return (
+    <>
+      <Head>
+        <title>Admin | Create Next App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Admin Page</h1>
+      </main>
+    </>
+  );
+}
